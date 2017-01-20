6b0d464821829bcb666d1ac32cdf2f372a8f939c
lin <java.com.lv@163.com>
2017-01-21 01:42:16 +0800

    滚动条

---
+++ app/app.scroll.ts
@@ -0,0 +1,85 @@
+//our root app component
+import {Component} from '@angular/core'
+
+@Component({
+    selector: 'my-app',
+    styles: [`
+    .search-results {
+      height: 100%;
+      // overflow: scroll;
+    }
+    .title {
+      position: fixed;
+      top: 0;
+      left: 0;
+      background-color: rgba(0,0,0,.5);
+      color: white;
+      width: 100%;
+    }
+    .title small {
+      color: #eaeaea;
+    }
+  `],
+    template: `
+    <h1 class="title well">{{ title }} <small>items: {{sum}}</small></h1>
+    <div class="search-results"
+         infinite-scroll
+         [infiniteScrollDistance]="scrollDistance"
+         [infiniteScrollThrottle]="throttle"
+         (scrolled)="onScrollDown()">
+      <p *ngFor="let i of array">
+        {{ i.name }}
+      </p>
+    </div>
+  `
+})
+export class AppComponent {
+    array : any;
+    sum = 100;
+    throttle = 300;
+    scrollDistance = 1;
+    title = 'Hello InfiniteScroll v0.2.9, Ng2 Final';
+
+    constructor() {
+        this.array = [
+            {"name":"zhangsan"},
+            {"name":"zhangsan"},
+            {"name":"zhangsan"},
+            {"name":"zhangsan"},
+            {"name":"zhangsan"},
+            {"name":"zhangsan"},{"name":"zhangsan"},
+            {"name":"zhangsan"},
+            {"name":"zhangsan"},
+            {"name":"zhangsan"},
+            {"name":"zhangsan"},
+            {"name":"zhangsan"},
+            {"name":"zhangsan"},
+            {"name":"zhangsan"},
+            {"name":"zhangsan"},
+            {"name":"zhangsan"},
+            {"name":"zhangsan"},
+            {"name":"zhangsan"},
+            {"name":"zhangsan"},
+            {"name":"zhangsan"},
+            {"name":"zhangsan"},
+            {"name":"zhangsan"},
+            {"name":"zhangsan"},
+            {"name":"zhangsan"},
+            {"name":"zhangsan"},{"name":"zhangsan"},
+
+
+        ];
+    }
+
+
+    onScrollDown () {
+        console.log('scrolled!!');
+        alert(1);
+
+
+    }
+
+    generateWord() {
+        //return chance.word();
+    }
+}
