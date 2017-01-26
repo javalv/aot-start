dc14aae60577afb5c21ff04dd7990199a09884d0
lvfang <java.com.lv@163.com>
2017-01-26 09:24:35 +0800

    conf 配置

--- copy-dist-files.js
+++ copy-dist-files.js
@@ -1,37 +1,21 @@
-
 var fs = require( 'fs' ),
     stat = fs.stat;
 
-
 //删除
 deleteFolderRecursive = function(path) {
-
   var files = [];
-
   if( fs.existsSync(path) ) {
-
     files = fs.readdirSync(path);
-
     files.forEach(function(file,index){
-
       var curPath = path + "/" + file;
-
       if(fs.statSync(curPath).isDirectory()) { // recurse
-
         deleteFolderRecursive(curPath);
-
       } else { // delete file
-
         fs.unlinkSync(curPath);
-
       }
-
     });
-
     fs.rmdirSync(path);
-
   }
-
 };
 
 /*
